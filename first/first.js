0775c9ef99e33fd98193aa6c3879674ce9afa814
Rudra swain <rudrasankar96@gmail.com>
2024-08-05 21:32:13 +0530

    first

---
+++ first/first.js
@@ -0,0 +1,35 @@
+// Varialbe declaration in js ->
+// 1 => var
+// 2 => let
+// 3 => const
+
+//! var
+var name;
+name = "rudra";
+var name;
+name = "badal";
+
+//! let
+let food;
+food = "mango";
+
+food = "apple";
+console.log(food);
+
+//! const
+const play = "cricket";
+console.log(play);
+
+// Data types
+
+// 1. String 'a' "aaa"
+// 2. Symbol
+// 3. Null
+// 4. Number 1,2,3,4,5
+// 5. Big Int 111111111111111111
+// 6. Bool true, false
+// 7. undefined
+// 8. Object
+
+var obj;
+console.log(obj);
